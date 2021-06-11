3b5f5469eaaf1f56e592296105524dd84188bd2e
Zihad <zihadmahiuddin@gmail.com>
2021-06-11 07:37:16 +0600

    Fixed slash commands

--- src/main/kotlin/xyz/pokecord/bot/modules/general/events/ReadyEvent.kt
+++ src/main/kotlin/xyz/pokecord/bot/modules/general/events/ReadyEvent.kt
@@ -55,7 +55,7 @@ class ReadyEvent : Event() {
         }
       } else {
         commandsData.forEach { commandData ->
-          module.bot.jda.upsertCommand(commandData)
+          module.bot.jda.upsertCommand(commandData).await()
         }
       }
     }
