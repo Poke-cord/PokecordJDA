3ea406286e319dd2cdb2786ced3a5b096c3320e0
Zihad <zihadmahiuddin@gmail.com>
2021-06-11 06:03:55 +0600

    Temporarily disabled TOS agreement

--- src/main/kotlin/xyz/pokecord/bot/core/structures/discord/CommandHandler.kt
+++ src/main/kotlin/xyz/pokecord/bot/core/structures/discord/CommandHandler.kt
@@ -61,25 +61,25 @@ class CommandHandler(val bot: Bot) : CoroutineEventListener {
 
     val userData = context.getUserData()
     if (userData.blacklisted) return
-    if (!userData.agreedToTerms) {
-      context.reply(
-        context.embedTemplates.normal(
-          context.translate(
-            "misc.embeds.rules.description",
-            mapOf(
-              "user" to context.author.asMention,
-              "tosUrl" to "https://pokecord.xyz/rules"
-            )
-          )
-        ).build(),
-        true
-      ).await()
-      val responseMessage = event.channel.awaitMessage(context.author)
-      if (!responseMessage.contentRaw.equals(context.author.id.asTrainerId.toInt().toString(16).reversed(), true)) {
-        return
-      }
-      context.bot.database.userRepository.setAgreedToTerms(context.getUserData())
-    }
+//    if (!userData.agreedToTerms) {
+//      context.reply(
+//        context.embedTemplates.normal(
+//          context.translate(
+//            "misc.embeds.rules.description",
+//            mapOf(
+//              "user" to context.author.asMention,
+//              "tosUrl" to "https://pokecord.xyz/rules"
+//            )
+//          )
+//        ).build(),
+//        true
+//      ).await()
+//      val responseMessage = event.channel.awaitMessage(context.author)
+//      if (!responseMessage.contentRaw.equals(context.author.id.asTrainerId.toInt().toString(16).reversed(), true)) {
+//        return
+//      }
+//      context.bot.database.userRepository.setAgreedToTerms(context.getUserData())
+//    }
 
     val hasRunningCommand = bot.cache.isRunningCommand(context.author.id)
     if (hasRunningCommand) {
@@ -240,25 +240,25 @@ class CommandHandler(val bot: Bot) : CoroutineEventListener {
 
     val userData = context.getUserData()
     if (userData.blacklisted) return
-    if (!userData.agreedToTerms) {
-      context.reply(
-        context.embedTemplates.normal(
-          context.translate(
-            "misc.embeds.rules.description",
-            mapOf(
-              "user" to context.author.asMention,
-              "tosUrl" to "https://pokecord.xyz/rules"
-            )
-          )
-        ).build(),
-        true
-      ).await()
-      val responseMessage = event.channel.awaitMessage(context.author)
-      if (!responseMessage.contentRaw.equals(context.author.id.asTrainerId.toInt().toString(16).reversed(), true)) {
-        return
-      }
-      context.bot.database.userRepository.setAgreedToTerms(context.getUserData())
-    }
+//    if (!userData.agreedToTerms) {
+//      context.reply(
+//        context.embedTemplates.normal(
+//          context.translate(
+//            "misc.embeds.rules.description",
+//            mapOf(
+//              "user" to context.author.asMention,
+//              "tosUrl" to "https://pokecord.xyz/rules"
+//            )
+//          )
+//        ).build(),
+//        true
+//      ).await()
+//      val responseMessage = event.channel.awaitMessage(context.author)
+//      if (!responseMessage.contentRaw.equals(context.author.id.asTrainerId.toInt().toString(16).reversed(), true)) {
+//        return
+//      }
+//      context.bot.database.userRepository.setAgreedToTerms(context.getUserData())
+//    }
 
     val hasRunningCommand = bot.cache.isRunningCommand(context.author.id)
     if (hasRunningCommand) {
