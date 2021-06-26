da3549e3808f44f6f80cab04e8fb45a2d259f800
Zihad <zihadmahiuddin@gmail.com>
2021-06-26 22:22:46 +0600

    Upgraded RemoteShardingKt to latest version

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,7 +41,7 @@ dependencies {
 
   implementation("com.github.minndevelopment:jda-ktx:${jdaKtxVersion}")
 
-  implementation("dev.zihad:remotesharding:1.0.1")
+  implementation("dev.zihad:remotesharding:1.0.2")
 
   implementation("net.dv8tion:JDA:$jdaVersion") {
 //  implementation("com.github.dv8fromtheworld:jda:development") { // JitPack
