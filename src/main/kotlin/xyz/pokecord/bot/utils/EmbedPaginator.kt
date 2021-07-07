19cd0e6628b7a99794e6708beaf5e3b258c8c7a1
Zihad <zihadmahiuddin@gmail.com>
2021-07-08 03:57:05 +0600

    Fixed embed paginator and confirmation button colors

--- src/main/kotlin/xyz/pokecord/bot/utils/EmbedPaginator.kt
+++ src/main/kotlin/xyz/pokecord/bot/utils/EmbedPaginator.kt
@@ -75,7 +75,7 @@ class EmbedPaginator(
       val result = context.addActionRows(
         ActionRow.of(
           NavigationOptions.values.map {
-            Button.primary(it.buttonId, Emoji.fromUnicode(it.emoji))
+            Button.secondary(it.buttonId, Emoji.fromUnicode(it.emoji))
 //            Button.secondary(it.buttonId, it.buttonText)
 //              .withEmoji(Emoji.fromUnicode(it.emoji))
           }
