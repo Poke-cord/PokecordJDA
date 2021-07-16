44b8bee49d5e315a761a022443ac7d64873492f5
Zihad <zihadmahiuddin@gmail.com>
2021-07-16 23:32:40 +0600

    Only generate slash command data list if REGISTER_SLASH_COMMANDS is enabled

--- src/main/kotlin/xyz/pokecord/bot/modules/general/events/ReadyEvent.kt
+++ src/main/kotlin/xyz/pokecord/bot/modules/general/events/ReadyEvent.kt
@@ -20,35 +20,35 @@ class ReadyEvent : Event() {
   override val name = "Ready"
 
   private suspend fun prepareSlashCommands(jda: JDA) {
-    val commandsData = module.bot.modules.map { (_, module) ->
-      module.commands.mapNotNull { command ->
-        if (command is DeveloperCommand) return@mapNotNull null // TODO: add check for mod commands
-        val commandDescription = I18n.translate(null, command.descriptionI18nKey, "")
-        if (commandDescription == "") return@mapNotNull null
-        val commandData = CommandData(
-          command.name.lowercase(),
-          commandDescription
-        )
-
-        val executorFunction =
-          command.javaClass.kotlin.memberFunctions.find { it.annotations.any { annotation -> annotation is Command.Executor } }!!
-
-        val parameters = executorFunction.parameters.filter { it.kind == KParameter.Kind.VALUE }
-
-        for (param in parameters) {
-          val commandArgumentAnnotation = param.findAnnotation<Command.Argument>() ?: continue
-          commandData.addOption(
-            param.asOptionType,
-            commandArgumentAnnotation.name.ifEmpty { param.name!! }.removeAccents(),
-            commandArgumentAnnotation.description,
-            !commandArgumentAnnotation.optional
-          )
-        }
-        commandData
-      }
-    }.flatten()
-
     if (System.getenv("REGISTER_SLASH_COMMANDS") != null) {
+      val commandsData = module.bot.modules.map { (_, module) ->
+        module.commands.mapNotNull { command ->
+          if (command is DeveloperCommand) return@mapNotNull null // TODO: add check for mod commands
+          val commandDescription = I18n.translate(null, command.descriptionI18nKey, "")
+          if (commandDescription == "") return@mapNotNull null
+          val commandData = CommandData(
+            command.name.lowercase(),
+            commandDescription
+          )
+
+          val executorFunction =
+            command.javaClass.kotlin.memberFunctions.find { it.annotations.any { annotation -> annotation is Command.Executor } }!!
+
+          val parameters = executorFunction.parameters.filter { it.kind == KParameter.Kind.VALUE }
+
+          for (param in parameters) {
+            val commandArgumentAnnotation = param.findAnnotation<Command.Argument>() ?: continue
+            commandData.addOption(
+              param.asOptionType,
+              commandArgumentAnnotation.name.ifEmpty { param.name!! }.removeAccents(),
+              commandArgumentAnnotation.description,
+              !commandArgumentAnnotation.optional
+            )
+          }
+          commandData
+        }
+      }.flatten()
+
       if (module.bot.devEnv) {
         module.bot.shardManager.getGuildById(Config.testingServer)?.let {
           commandsData.forEach { commandData ->
