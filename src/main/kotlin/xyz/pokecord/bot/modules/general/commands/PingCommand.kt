d9e743278c796dcd1fe90124785fa08d741f9d8e
Zihad <zihadmahiuddin@gmail.com>
2021-07-16 06:09:45 +0600

    Changed ping command to use JDA.getRestPing

--- src/main/kotlin/xyz/pokecord/bot/modules/general/commands/PingCommand.kt
+++ src/main/kotlin/xyz/pokecord/bot/modules/general/commands/PingCommand.kt
@@ -1,13 +1,8 @@
 package xyz.pokecord.bot.modules.general.commands
 
 import dev.minn.jda.ktx.await
-import net.dv8tion.jda.api.entities.Message
-import net.dv8tion.jda.api.interactions.InteractionHook
-import xyz.pokecord.bot.core.structures.discord.MessageCommandContext
-import xyz.pokecord.bot.core.structures.discord.SlashCommandContext
 import xyz.pokecord.bot.core.structures.discord.base.BaseCommandContext
 import xyz.pokecord.bot.core.structures.discord.base.Command
-import java.time.temporal.ChronoUnit
 
 class PingCommand : Command() {
   override val name = "Ping"
@@ -16,30 +11,17 @@ class PingCommand : Command() {
   suspend fun execute(
     context: BaseCommandContext
   ) {
-    val embedTitle = context.translate("modules.general.commands.ping.embed.title")
-    var embedDescription = context.translate(
-      "modules.general.commands.ping.embed.description",
-      mapOf(
-        "apiPing" to "${context.jda.gatewayPing}ms",
-        "ping" to "Calculating..."
-      )
-    )
-
-    val result =
-      context.reply(context.embedTemplates.normal(embedDescription, embedTitle).build()).await()
-    val message = when (context) {
-      is MessageCommandContext -> result as Message
-      is SlashCommandContext -> (result as InteractionHook).retrieveOriginal().await()
-      else -> throw IllegalStateException("Unknown command context type ${context::class.java.name}")
-    }
-    embedDescription = context.translate(
-      "modules.general.commands.ping.embed.description",
-      mapOf(
-        "apiPing" to "${context.jda.gatewayPing}ms",
-        "ping" to "${context.timeCreated.until(message.timeCreated, ChronoUnit.MILLIS)}ms"
-      )
-    )
-    message.editMessageEmbeds(context.embedTemplates.normal(embedDescription, embedTitle).build())
-      .queue()
+    context.reply(
+      context.embedTemplates.normal(
+        context.translate(
+          "modules.general.commands.ping.embed.description",
+          mapOf(
+            "apiPing" to "${context.jda.gatewayPing}ms",
+            "ping" to "${context.jda.restPing.await()}ms"
+          )
+        ),
+        context.translate("modules.general.commands.ping.embed.title")
+      ).build()
+    ).queue()
   }
 }
