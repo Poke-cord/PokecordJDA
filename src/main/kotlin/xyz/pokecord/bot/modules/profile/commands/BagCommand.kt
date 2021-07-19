e1a3241c721074e63bf31e24ccc5f8741e2181e4
Zihad <zihadmahiuddin@gmail.com>
2021-07-20 00:37:22 +0600

    Fixed staff check in BagCommand

--- src/main/kotlin/xyz/pokecord/bot/modules/profile/commands/BagCommand.kt
+++ src/main/kotlin/xyz/pokecord/bot/modules/profile/commands/BagCommand.kt
@@ -21,7 +21,7 @@ class BagCommand : Command() {
     val checkingSelf = user == null
 
     val userData = if (checkingSelf) context.getUserData() else module.bot.database.userRepository.getUser(targetUser)
-    if (!context.isStaff() || userData.progressPrivate && !checkingSelf) {
+    if (!context.isStaff() && userData.progressPrivate && !checkingSelf) {
       context.reply(context.embedTemplates.progressPrivate(targetUser).build()).queue()
       return
     }
