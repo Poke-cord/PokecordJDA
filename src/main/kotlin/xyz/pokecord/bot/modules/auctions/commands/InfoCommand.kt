30086d4f09c6952173701565c06a789c5e95be68
Zihad <zihadmahiuddin@gmail.com>
2022-01-01 21:56:40 +0600

    Fixed auction and market info commands

--- src/main/kotlin/xyz/pokecord/bot/modules/auctions/commands/InfoCommand.kt
+++ src/main/kotlin/xyz/pokecord/bot/modules/auctions/commands/InfoCommand.kt
@@ -3,9 +3,10 @@ package xyz.pokecord.bot.modules.auctions.commands
 import xyz.pokecord.bot.api.ICommandContext
 import xyz.pokecord.bot.core.structures.discord.base.Command
 import xyz.pokecord.bot.core.structures.pokemon.Pokemon
+import xyz.pokecord.bot.core.structures.pokemon.Stat
 import xyz.pokecord.bot.utils.extensions.humanizeMs
 
-object InfoCommand: Command() {
+object InfoCommand : Command() {
   override val name = "Info"
 
   @Executor
@@ -13,9 +14,9 @@ object InfoCommand: Command() {
     context: ICommandContext,
     @Argument auctionId: Int?
   ) {
-    if(!context.hasStarted(true)) return
+    if (!context.hasStarted(true)) return
 
-    if(auctionId == null) {
+    if (auctionId == null) {
       context.reply(
         context.embedTemplates.error(
           context.translate("modules.auctions.commands.info.errors.noAuctionId")
@@ -25,7 +26,7 @@ object InfoCommand: Command() {
     }
 
     val auction = context.bot.database.auctionRepository.getAuction(auctionId)
-    if(auction == null) {
+    if (auction == null) {
       context.reply(
         context.embedTemplates.error(
           context.translate("modules.auctions.commands.info.errors.noAuctionFound", "id" to auctionId.toString())
@@ -33,36 +34,37 @@ object InfoCommand: Command() {
       ).queue()
     } else {
       val auctionPokemon = context.bot.database.pokemonRepository.getPokemonById(auction.pokemon)
-      if(auctionPokemon != null) {
+      if (auctionPokemon != null) {
         val pokemon = Pokemon.getById(auctionPokemon.id)
-        val highestBid = auction.highestBid
-        val xp = if(auctionPokemon.level >= 100) "Max" else auctionPokemon.xp.toString() + "/" + auctionPokemon.requiredXpToLevelUp().toString() + "xp"
+
+        val infoSection = """
+        **${context.translate("misc.texts.auctionId")}**: ${auction.id}
+        **${context.translate("misc.texts.startingBid")}**: ${auction.startingBid}
+        **${context.translate("misc.texts.bidCount")}**: ${auction.bids.size}
+        **${context.translate("misc.texts.timeLeft")}**: ${auction.timeLeft.humanizeMs()}
+        **${context.translate("misc.texts.currentBid")}**: ${auction.highestBid?.amount ?: context.translate("misc.texts.noBids")}
+        
+        **${context.translate("misc.texts.xp")}**: ${if (auctionPokemon.level >= 100) "Max" else "${auctionPokemon.xp}/${auctionPokemon.requiredXpToLevelUp()}"}
+        **${context.translate("misc.texts.gender")}**: ${context.translator.gender(auctionPokemon)}
+        **${context.translate("misc.texts.nature")}**: ${context.translator.nature(auctionPokemon.nature)}
+        """.trimIndent()
+
+        val statSection = """
+        **${context.translator.stat(Stat.hp)}**: ${auctionPokemon.stats.hp} - ${auctionPokemon.ivs.hp}/31
+        **${context.translator.stat(Stat.attack)}**: ${auctionPokemon.stats.attack} - ${auctionPokemon.ivs.attack}/31
+        **${context.translator.stat(Stat.defense)}**: ${auctionPokemon.stats.defense} - ${auctionPokemon.ivs.defense}/31
+        **${context.translator.stat(Stat.specialAttack)}**: ${auctionPokemon.stats.specialAttack} - ${auctionPokemon.ivs.specialAttack}/31
+        **${context.translator.stat(Stat.specialDefense)}**: ${auctionPokemon.stats.specialDefense} - ${auctionPokemon.ivs.specialDefense}/31
+        **${context.translator.stat(Stat.speed)}**: ${auctionPokemon.stats.speed} - ${auctionPokemon.ivs.speed}/31
+        **${context.translate("misc.texts.totalIv")}**: ${auctionPokemon.ivPercentage}
+        """.trimIndent()
 
         context.reply(
           context.embedTemplates
             .normal(
+              infoSection + "\n" + statSection,
               context.translate(
-                "modules.auctions.commands.info.displayInfo.description",
-                mapOf(
-                  "auctionId" to auction.id.toString(),
-                  "startingBid" to auction.startingBid.toString(),
-                  "bidCount" to auction.bids.size.toString(),
-                  "timeLeft" to auction.timeLeft.humanizeMs(),
-                  "currentBid" to (highestBid?.amount?.toString() ?: context.translate("misc.texts.noBids")),
-                  "XP" to xp,
-                  "gender" to context.translator.gender(auctionPokemon.gender),
-                  "nature" to context.translator.nature(auctionPokemon.nature)!!,
-                  "hp" to (auctionPokemon.stats.hp - auctionPokemon.ivs.hp).toString(),
-                  "attack" to (auctionPokemon.stats.attack - auctionPokemon.ivs.attack).toString(),
-                  "defence" to (auctionPokemon.stats.defense - auctionPokemon.ivs.defense).toString(),
-                  "special-attack" to (auctionPokemon.stats.specialAttack - auctionPokemon.ivs.specialAttack).toString(),
-                  "special-defense" to (auctionPokemon.stats.specialDefense - auctionPokemon.ivs.specialDefense).toString(),
-                  "speed" to (auctionPokemon.stats.speed - auctionPokemon.ivs.speed).toString(),
-                  "ivPercentage" to auctionPokemon.ivPercentage
-                )
-              ),
-              context.translate(
-                "modules.auctions.commands.info.displayInfo.title",
+                "modules.auctions.commands.info.title",
                 mapOf(
                   "pokemonLevel" to auctionPokemon.level.toString(),
                   "pokemonName" to context.translator.pokemonDisplayName(auctionPokemon),
--- src/main/kotlin/xyz/pokecord/bot/modules/market/commands/InfoCommand.kt
+++ src/main/kotlin/xyz/pokecord/bot/modules/market/commands/InfoCommand.kt
@@ -3,8 +3,9 @@ package xyz.pokecord.bot.modules.market.commands
 import xyz.pokecord.bot.api.ICommandContext
 import xyz.pokecord.bot.core.structures.discord.base.Command
 import xyz.pokecord.bot.core.structures.pokemon.Pokemon
+import xyz.pokecord.bot.core.structures.pokemon.Stat
 
-object InfoCommand: Command() {
+object InfoCommand : Command() {
   override val name = "Info"
 
   @Executor
@@ -12,9 +13,9 @@ object InfoCommand: Command() {
     context: ICommandContext,
     @Argument listingId: Int?
   ) {
-    if(!context.hasStarted(true)) return
+    if (!context.hasStarted(true)) return
 
-    if(listingId == null) {
+    if (listingId == null) {
       context.reply(
         context.embedTemplates.error(
           context.translate("modules.market.commands.info.errors.noListingId")
@@ -24,7 +25,7 @@ object InfoCommand: Command() {
     }
 
     val listing = context.bot.database.marketRepository.getListing(listingId)
-    if(listing == null) {
+    if (listing == null) {
       context.reply(
         context.embedTemplates.error(
           context.translate("modules.market.commands.info.errors.noListingFound", "id" to listingId.toString())
@@ -32,32 +33,34 @@ object InfoCommand: Command() {
       ).queue()
     } else {
       val listingPokemon = context.bot.database.pokemonRepository.getPokemonById(listing.pokemon)
-      if(listingPokemon != null) {
+      if (listingPokemon != null) {
         val pokemon = Pokemon.getById(listingPokemon.id)
-        val xp = if(listingPokemon.level >= 100) "Max" else listingPokemon.xp.toString() + "/" + listingPokemon.requiredXpToLevelUp().toString() + "xp"
+
+        val infoSection = """
+        **${context.translate("misc.texts.listingId")}**: ${listing.id}
+        **${context.translate("misc.texts.price")}**: ${listing.price}
+        
+        **${context.translate("misc.texts.xp")}**: ${if (listingPokemon.level >= 100) "Max" else "${listingPokemon.xp}/${listingPokemon.requiredXpToLevelUp()}"}
+        **${context.translate("misc.texts.gender")}**: ${context.translator.gender(listingPokemon)}
+        **${context.translate("misc.texts.nature")}**: ${context.translator.nature(listingPokemon.nature)}
+        """.trimIndent()
+
+        val statSection = """
+        **${context.translator.stat(Stat.hp)}**: ${listingPokemon.stats.hp} - ${listingPokemon.ivs.hp}/31
+        **${context.translator.stat(Stat.attack)}**: ${listingPokemon.stats.attack} - ${listingPokemon.ivs.attack}/31
+        **${context.translator.stat(Stat.defense)}**: ${listingPokemon.stats.defense} - ${listingPokemon.ivs.defense}/31
+        **${context.translator.stat(Stat.specialAttack)}**: ${listingPokemon.stats.specialAttack} - ${listingPokemon.ivs.specialAttack}/31
+        **${context.translator.stat(Stat.specialDefense)}**: ${listingPokemon.stats.specialDefense} - ${listingPokemon.ivs.specialDefense}/31
+        **${context.translator.stat(Stat.speed)}**: ${listingPokemon.stats.speed} - ${listingPokemon.ivs.speed}/31
+        **${context.translate("misc.texts.totalIv")}**: ${listingPokemon.ivPercentage}
+        """.trimIndent()
 
         context.reply(
           context.embedTemplates
             .normal(
+              infoSection + "\n" + statSection,
               context.translate(
-                "modules.market.commands.info.displayInfo.description",
-                mapOf(
-                  "listingId" to listing.id.toString(),
-                  "price" to listing.price.toString(),
-                  "XP" to xp,
-                  "gender" to context.translator.gender(listingPokemon.gender),
-                  "nature" to context.translator.nature(listingPokemon.nature)!!,
-                  "hp" to (listingPokemon.stats.hp - listingPokemon.ivs.hp).toString(),
-                  "attack" to (listingPokemon.stats.attack - listingPokemon.ivs.attack).toString(),
-                  "defence" to (listingPokemon.stats.defense - listingPokemon.ivs.defense).toString(),
-                  "special-attack" to (listingPokemon.stats.specialAttack - listingPokemon.ivs.specialAttack).toString(),
-                  "special-defense" to (listingPokemon.stats.specialDefense - listingPokemon.ivs.specialDefense).toString(),
-                  "speed" to (listingPokemon.stats.speed - listingPokemon.ivs.speed).toString(),
-                  "ivPercentage" to listingPokemon.ivPercentage
-                )
-              ),
-              context.translate(
-                "modules.market.commands.info.displayInfo.title",
+                "modules.market.commands.info.title",
                 mapOf(
                   "pokemonLevel" to listingPokemon.level.toString(),
                   "pokemonName" to context.translator.pokemonDisplayName(listingPokemon),
