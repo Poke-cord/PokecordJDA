10a10531a979483292c0969b3deeac91aa090c3e
Dep0sit <ashkarmalek0@gmail.com>
2022-01-20 11:49:21 -0500

    :sparkles: Added support for subcommands on slash commands

--- src/main/kotlin/xyz/pokecord/bot/modules/general/events/ReadyEvent.kt
+++ src/main/kotlin/xyz/pokecord/bot/modules/general/events/ReadyEvent.kt
@@ -4,6 +4,7 @@ import dev.minn.jda.ktx.await
 import net.dv8tion.jda.api.JDA
 import net.dv8tion.jda.api.events.ReadyEvent
 import net.dv8tion.jda.api.interactions.commands.build.CommandData
+import net.dv8tion.jda.api.interactions.commands.build.SubcommandData
 import xyz.pokecord.bot.core.managers.I18n
 import xyz.pokecord.bot.core.structures.discord.base.Command
 import xyz.pokecord.bot.core.structures.discord.base.Event
@@ -20,34 +21,71 @@ import kotlin.reflect.full.memberFunctions
 object ReadyEvent : Event() {
   override val name = "Ready"
 
+  private fun prepareSubcommandData(command: Command): SubcommandData? {
+    if (command is DeveloperCommand) return null // TODO: add check for mod commands
+    val commandDescription = I18n.translate(null, command.descriptionI18nKey, "")
+    if (commandDescription == "") return null
+    val commandData = SubcommandData(
+      command.name.lowercase(),
+      commandDescription
+    )
+
+    val executorFunction =
+      command.javaClass.kotlin.memberFunctions.find { it.annotations.any { annotation -> annotation is Command.Executor } }!!
+
+    val parameters = executorFunction.parameters.filter { it.kind == KParameter.Kind.VALUE }
+
+    for (param in parameters) {
+      val commandArgumentAnnotation = param.findAnnotation<Command.Argument>() ?: continue
+      commandData.addOption(
+        param.asOptionType,
+        commandArgumentAnnotation.name.ifEmpty { param.name!! }.removeAccents(),
+        commandArgumentAnnotation.description,
+        !commandArgumentAnnotation.optional
+      )
+    }
+
+    return commandData
+  }
+
+  private fun prepareCommandData(command: Command): CommandData? {
+    if (command is DeveloperCommand) return null // TODO: add check for mod commands
+    val commandDescription = I18n.translate(null, command.descriptionI18nKey, "")
+    if (commandDescription == "") return null
+    val commandData = CommandData(
+      command.name.lowercase(),
+      commandDescription
+    )
+
+    val executorFunction =
+      command.javaClass.kotlin.memberFunctions.find { it.annotations.any { annotation -> annotation is Command.Executor } }!!
+
+    val parameters = executorFunction.parameters.filter { it.kind == KParameter.Kind.VALUE }
+
+    if(command is ParentCommand) {
+      for (subcommand in command.childCommands) {
+        val subcommandData = prepareSubcommandData(subcommand)
+        commandData.addSubcommands(subcommandData)
+      }
+    }
+
+    for (param in parameters) {
+      val commandArgumentAnnotation = param.findAnnotation<Command.Argument>() ?: continue
+      commandData.addOption(
+        param.asOptionType,
+        commandArgumentAnnotation.name.ifEmpty { param.name!! }.removeAccents(),
+        commandArgumentAnnotation.description,
+        !commandArgumentAnnotation.optional
+      )
+    }
+
+    return commandData
+  }
+
   private suspend fun prepareSlashCommands(jda: JDA) {
     if (System.getenv("REGISTER_SLASH_COMMANDS") != null) {
       val commandsData = module.bot.modules.map { (_, module) ->
-        module.commands.mapNotNull { command ->
-          if (command is DeveloperCommand) return@mapNotNull null // TODO: add check for mod commands
-          val commandDescription = I18n.translate(null, command.descriptionI18nKey, "")
-          if (commandDescription == "") return@mapNotNull null
-          val commandData = CommandData(
-            command.name.lowercase(),
-            commandDescription
-          )
-
-          val executorFunction =
-            command.javaClass.kotlin.memberFunctions.find { it.annotations.any { annotation -> annotation is Command.Executor } }!!
-
-          val parameters = executorFunction.parameters.filter { it.kind == KParameter.Kind.VALUE }
-
-          for (param in parameters) {
-            val commandArgumentAnnotation = param.findAnnotation<Command.Argument>() ?: continue
-            commandData.addOption(
-              param.asOptionType,
-              commandArgumentAnnotation.name.ifEmpty { param.name!! }.removeAccents(),
-              commandArgumentAnnotation.description,
-              !commandArgumentAnnotation.optional
-            )
-          }
-          commandData
-        }
+        module.commands.mapNotNull { command -> prepareCommandData(command) }
       }.flatten()
 
       if (module.bot.devEnv) {
