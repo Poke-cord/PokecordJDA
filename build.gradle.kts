4e8284427c6c625f37fa153ac03a422e37abc5f4
Mystic <80657250+MysticFyr3@users.noreply.github.com>
2022-06-25 19:12:55 -0700

    Updated version 2.0.0 >>> 2.1.1
    Pokémon spawn level changes - 2.1.0 - Initial commit of economy changes
    Formatting changes - 2.1.1 - Minor changes to text display for auctions, duels, and market
    Release module; Nature Candy; Ranges for Add; EV system and items - 2.1.5 - Pending release
    Functionality for multiple item use at once - 2.1.6 - Pending development

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
 group = "xyz.pokecord"
-version = "2.0.0"
+version = "2.1.1"
 
 val discordWebhooksVersion = "0.7.4"
 val graalJsVersion = "21.2.0"
