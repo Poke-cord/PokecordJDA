23b00e3163c5761868af9f5bc449b88dea2498ba
Mystic <80657250+MysticFyr3@users.noreply.github.com>
2022-07-04 16:43:33 -0700

    Updated version 2.1.53 >>> 2.1.59

--- src/main/kotlin/xyz/pokecord/bot/utils/Config.kt
+++ src/main/kotlin/xyz/pokecord/bot/utils/Config.kt
@@ -5,7 +5,7 @@ import java.time.LocalDate
 import java.time.ZoneOffset
 
 object Config {
-  const val version = "2.1.53"
+  const val version = "2.1.59"
 
   val voteSeasonStartDate: LocalDate = Instant.ofEpochMilli(1609459200000).atZone(ZoneOffset.UTC).toLocalDate()
 
