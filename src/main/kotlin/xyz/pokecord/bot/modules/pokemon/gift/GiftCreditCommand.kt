a39cb9744eaae3db85bb743376f5a90f4b4cb40d
Mystic <80657250+MysticFyr3@users.noreply.github.com>
2022-07-04 04:09:04 -0700

    Attempted fix for making p!gift c staff command

--- src/main/kotlin/xyz/pokecord/bot/modules/pokemon/gift/GiftCreditCommand.kt
+++ src/main/kotlin/xyz/pokecord/bot/modules/pokemon/gift/GiftCreditCommand.kt
@@ -5,6 +5,7 @@ import net.dv8tion.jda.api.entities.User
 import xyz.pokecord.bot.api.ICommandContext
 import xyz.pokecord.bot.core.structures.discord.base.Command
 import xyz.pokecord.bot.modules.pokemon.commands.GiftCommand
+import xyz.pokecord.bot.modules.staff.StaffCommand
 import xyz.pokecord.bot.utils.Confirmation
 
 object GiftCreditCommand : StaffCommand() {
