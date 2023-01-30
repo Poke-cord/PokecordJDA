c940f7a52a08d2525edcd5badf3485f7abca1a8f
Mystic <80657250+MysticFyr3@users.noreply.github.com>
2023-01-29 23:49:49 -0800

    Updated au info stats format to match pk info

--- src/main/kotlin/xyz/pokecord/bot/modules/auctions/commands/InfoCommand.kt
+++ src/main/kotlin/xyz/pokecord/bot/modules/auctions/commands/InfoCommand.kt
@@ -38,38 +38,41 @@ object InfoCommand : Command() {
       if (auctionPokemon != null) {
         val pokemon = Pokemon.getById(auctionPokemon.id)
 
-        val infoSection = """
+        val auInfoSection = """
         **${context.translate("misc.texts.auctionId")}**: ${auction.id}
-        **${context.translate("misc.texts.startingBid")}**: ${context.translator.numberFormat(auction.startingBid)}
         **${context.translate("misc.texts.bidCount")}**: ${auction.bids.size}
-        **${context.translate("misc.texts.timeLeft")}**: ${auction.timeLeft.humanizeMs()}
-        **${context.translate("misc.texts.currentBid")}**: ${
+        **${context.translate("misc.texts.startingCurrentBid")}**
+        > ${context.translator.numberFormat(auction.startingBid)} / ${
           auction.highestBid?.amount?.let {
             context.translator.numberFormat(
               it
             )
           } ?: context.translate("misc.texts.noBids")
         }
-        
+        **${context.translate("misc.texts.timeLeft")}**: ${auction.timeLeft.humanizeMs()}
+        """.trimIndent()
+
+        val infoSection = """
         **${context.translate("misc.texts.xp")}**: ${if (auctionPokemon.level >= 100) "Max" else "${auctionPokemon.xp}/${auctionPokemon.requiredXpToLevelUp()}"}
         **${context.translate("misc.texts.gender")}**: ${context.translator.gender(auctionPokemon)}
         **${context.translate("misc.texts.nature")}**: ${context.translator.nature(auctionPokemon.nature)}
         """.trimIndent()
 
         val statSection = """
-        **${context.translator.stat(Stat.hp)}**: ${auctionPokemon.stats.hp} - ${auctionPokemon.ivs.hp}/31
-        **${context.translator.stat(Stat.attack)}**: ${auctionPokemon.stats.attack} - ${auctionPokemon.ivs.attack}/31
-        **${context.translator.stat(Stat.defense)}**: ${auctionPokemon.stats.defense} - ${auctionPokemon.ivs.defense}/31
-        **${context.translator.stat(Stat.specialAttack)}**: ${auctionPokemon.stats.specialAttack} - ${auctionPokemon.ivs.specialAttack}/31
-        **${context.translator.stat(Stat.specialDefense)}**: ${auctionPokemon.stats.specialDefense} - ${auctionPokemon.ivs.specialDefense}/31
-        **${context.translator.stat(Stat.speed)}**: ${auctionPokemon.stats.speed} - ${auctionPokemon.ivs.speed}/31
+        **Stat Spread (IV, +EV)**
+        `${context.translate("misc.texts.hp").padEnd(7)}| ${auctionPokemon.stats.hp.toString().padStart(3)} | ${auctionPokemon.ivs.hp.toString().padStart(2)}/31${ if(auctionPokemon.evs.hp > 0) ("+" + (auctionPokemon.evs.hp).toString()).padStart(6) else " ".repeat(6)}`
+        `${context.translate("misc.texts.attack").padEnd(7)}| ${auctionPokemon.stats.attack.toString().padStart(3)} | ${auctionPokemon.ivs.attack.toString().padStart(2)}/31${ if(auctionPokemon.evs.attack > 0) ("+" + (auctionPokemon.evs.attack).toString()).padStart(6) else " ".repeat(6)}`
+        `${context.translate("misc.texts.defense").padEnd(7)}| ${auctionPokemon.stats.defense.toString().padStart(3)} | ${auctionPokemon.ivs.defense.toString().padStart(2)}/31${ if(auctionPokemon.evs.defense > 0) ("+" + (auctionPokemon.evs.defense).toString()).padStart(6) else " ".repeat(6)}`
+        `${context.translate("misc.texts.specialAttack").padEnd(7)}| ${auctionPokemon.stats.specialAttack.toString().padStart(3)} | ${auctionPokemon.ivs.specialAttack.toString().padStart(2)}/31${ if(auctionPokemon.evs.specialAttack > 0) ("+" + (auctionPokemon.evs.specialAttack).toString()).padStart(6) else " ".repeat(6)}`
+        `${context.translate("misc.texts.specialDefense").padEnd(7)}| ${auctionPokemon.stats.specialDefense.toString().padStart(3)} | ${auctionPokemon.ivs.specialDefense.toString().padStart(2)}/31${ if(auctionPokemon.evs.specialDefense > 0) ("+" + (auctionPokemon.evs.specialDefense).toString()).padStart(6) else " ".repeat(6)}`
+        `${context.translate("misc.texts.speed").padEnd(7)}| ${auctionPokemon.stats.speed.toString().padStart(3)} | ${auctionPokemon.ivs.speed.toString().padStart(2)}/31${ if(auctionPokemon.evs.speed > 0) ("+" + (auctionPokemon.evs.speed).toString()).padStart(6) else " ".repeat(6)}`
         **${context.translate("misc.texts.totalIv")}**: ${auctionPokemon.ivPercentage}
         """.trimIndent()
 
         context.reply(
           context.embedTemplates
             .normal(
-              infoSection + "\n" + statSection,
+              auInfoSection + "\n\n" + infoSection + "\n\n" + statSection,
               context.translate(
                 "modules.auctions.commands.info.title",
                 mapOf(
