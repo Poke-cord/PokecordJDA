73d017313b894c2ae68e87c4563481062de8d056
Zihad <zihadmahiuddin@gmail.com>
2023-03-26 05:39:34 +0600

    remove unused imports

--- src/main/kotlin/xyz/pokecord/App.kt
+++ src/main/kotlin/xyz/pokecord/App.kt
@@ -9,8 +9,6 @@ import net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder
 import net.dv8tion.jda.api.utils.cache.CacheFlag
 import org.slf4j.LoggerFactory
 import xyz.pokecord.bot.core.structures.discord.Bot
-import xyz.pokecord.bot.core.structures.pokemon.Pokemon
-import xyz.pokecord.bot.core.structures.pokemon.PokemonForm
 import xyz.pokecord.bot.modules.auctions.AuctionsModule
 import xyz.pokecord.bot.modules.battle.BattleModule
 import xyz.pokecord.bot.modules.developer.DeveloperModule
