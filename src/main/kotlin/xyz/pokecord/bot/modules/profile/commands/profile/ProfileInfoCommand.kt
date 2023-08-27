ebca119a97475fa286317a4ba076826673067efa
MysticFyr3 <mysticfyr3@gmail.com>
2023-08-27 04:49:45 +0000

    Added shiny rate to profile info command

--- src/main/kotlin/xyz/pokecord/bot/modules/profile/commands/profile/ProfileInfoCommand.kt
+++ src/main/kotlin/xyz/pokecord/bot/modules/profile/commands/profile/ProfileInfoCommand.kt
@@ -1,31 +1,29 @@
 package xyz.pokecord.bot.modules.profile.commands.profile
 
-import net.dv8tion.jda.api.EmbedBuilder
 import xyz.pokecord.bot.api.ICommandContext
-import xyz.pokecord.bot.core.structures.discord.EmbedTemplates
 import xyz.pokecord.bot.core.structures.discord.base.Command
-import xyz.pokecord.bot.core.structures.pokemon.Pokemon
-import xyz.pokecord.bot.core.managers.database.repositories.PokemonRepository
-import xyz.pokecord.bot.utils.Config
-import xyz.pokecord.bot.utils.EmbedPaginator
 
 //This command to eventually merge with @pkc profile.
 
 object ProfileInfoCommand : Command() {
   override val name = "Info"
 
-  override var aliases = arrayOf("i", "stat", "stats")
+  override var aliases = arrayOf("i", "s", "stat", "stats")
 
   @Executor
   suspend fun execute(
     context: ICommandContext
   ) {
     val pokemonCount = module.bot.database.pokemonRepository.getPokemonCount(context.author.id)
+    val shinyRate = module.bot.database.userRepository.getUser(context.author.id).shinyRate
+
     context.reply(
       context.embedTemplates.normal(
         context.translate("modules.profile.commands.profile.embed.info.description",
           mapOf(
-            "count" to pokemonCount.toString()
+//            "trainerId" to trainerId,
+            "count" to pokemonCount.toString(),
+            "rate" to shinyRate.toString()
           )
         ),
         context.translate("modules.profile.commands.profile.embed.info.title",
