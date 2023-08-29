94fae17d6bc07e37a9d55dc448e115c1ec6cc7f7
ADH <amritpalvirk36@gmail.com>
2023-08-30 02:24:21 +0530

    Update BreedCommand.kt
    1.2 -
    added condition to check null

--- src/main/kotlin/xyz/pokecord/bot/modules/nursery/commands/BreedCommand.kt
+++ src/main/kotlin/xyz/pokecord/bot/modules/nursery/commands/BreedCommand.kt
@@ -2,10 +2,27 @@ package xyz.pokecord.bot.modules.nursery.commands
 
 import xyz.pokecord.bot.core.structures.discord.base.Command
 import xyz.pokecord.bot.modules.nursery.commands.PokemonBreeder
+import xyz.pokecord.bot.api.ICommandContext
 
 class BreedCommand : Command() {
   override val name = "breed"
-  suspend fun execute() {
+  suspend fun execute(
+
+    context: ICommandContext,
+    @Argument  (name = "pokemon") pokemonName: String?,
+    @Argument  (name = "pokemon") pokemonName2: String?
+  ) {
+    if (pokemonName == null) {
+      context.reply(context.embedTemplates.error(context.translate("misc.errors.pokemonNotFound")).build())
+        .queue()
+      return
+    }
+    else if (pokemonName2 == null) {
+      context.reply(context.embedTemplates.error(context.translate("misc.errors.pokemonNotFound")).build())
+        .queue()
+      return
+    }
+
     val pokemon1Id = args[0]
     val pokemon2Id = args[1]
 
