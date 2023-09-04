aff562d6844f9fbd1cb6c5833b65b462bdd0fc94
ADH <amritpalvirk36@gmail.com>
2023-09-04 23:25:11 +0530

    Update DaycareRepository.kt

--- src/main/kotlin/xyz/pokecord/bot/core/managers/database/repositories/DaycareRepository.kt
+++ src/main/kotlin/xyz/pokecord/bot/core/managers/database/repositories/DaycareRepository.kt
@@ -1,11 +1,19 @@
 package xyz.pokecord.bot.core.managers.database.repositories
 
+import org.litote.kmongo.KMongo
+import org.litote.kmongo.coroutine.CoroutineCollection
 import org.litote.kmongo.eq
+import xyz.pokecord.bot.core.managers.database.Database
+import xyz.pokecord.bot.core.managers.database.models.VoteReward
 import xyz.pokecord.bot.core.structures.pokemon.Pokemon
 
-class DaycareRepository {
+class DaycareRepository(
+  database: Database,private val voteRewardCollection: CoroutineCollection<daycare>
+): Repository(database){
 
-  private val collection = KMongoCollection.getCollection<Pokemon>("daycare")
+  private val collection = KMongo
+
+  getCollection<Pokemon>("daycare")
 
   suspend fun addPokemon(pokemon: Pokemon) {
     collection.insertOne(pokemon)
