bd469a656fe7305b85fea113df55d8cb7f8ebb4f
MysticFyr3 <80657250+MysticFyr3@users.noreply.github.com>
2024-05-13 16:42:57 -0700

    Continue event

--- src/main/kotlin/xyz/pokecord/bot/core/structures/pokemon/SpecialEvents.kt
+++ src/main/kotlin/xyz/pokecord/bot/core/structures/pokemon/SpecialEvents.kt
@@ -249,7 +249,7 @@ object SpecialEvents {
           ),
         )
       ),
-      /*
+
       EventPokemon( //mayflowers 2024 week 2 - mother's day
         "Mayflowers 2024",
         parseDateTime("2024-05-13 07:00:00"),
@@ -344,7 +344,7 @@ object SpecialEvents {
             Pokemon.getByName("Awareness Mr. Rime V2")!!
           ),
         )
-      ),
+      ),/*
       EventPokemon( //mayflowers 2024 week 3 - space
         "Mayflowers 2024",
         parseDateTime("2024-05-20 07:00:00"),
